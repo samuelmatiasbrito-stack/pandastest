ffdc508cacd203fd4a6389240c6a18cd0b6431b9
Samuel Matias <samuelmatiasbrito@gmail.com>
2026-02-10 21:50:48 -0300

    sem cabeçalho da planilha

--- pandastest.py
+++ pandastest.py
@@ -3,6 +3,5 @@ df = pd.read_excel('polosaomiguel.xlsx')
 # print(df)
 BonusVip = df[(df['ponto gera'] > 1400) & (df['nível'] == 'VIP')]
 BonusMp = df[(df['ponto gera'] > 1000) & (df['nível'] == 'MP')]
-
-print(f' Os executivos MP que bateram o bonus são: {BonusMp[['executivos']]}')
-print(f'VIPs que ganharam bonus: {BonusVip[['executivos']]}')
+print(f' Os executivos MP que bateram o bonus são: {BonusMp[['executivos', 'ponto gera']].to_string(header=False)}')
+print(f'VIPs que ganharam bonus: {BonusVip[['executivos', 'ponto gera']].to_string(header=False)}')
