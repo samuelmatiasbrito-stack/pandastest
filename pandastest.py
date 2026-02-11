eefd1d0f0eedf665d68e9e28f80c5e806e09f4dd
Samuel Matias <samuelmatiasbrito@gmail.com>
2026-02-11 16:51:31 -0300

    bonus gera + bonus M1

--- pandastest.py
+++ pandastest.py
@@ -1,7 +1,40 @@
 import pandas as pd
 df = pd.read_excel('polosaomiguel.xlsx')
-# print(df)
-BonusVip = df[(df['ponto gera'] > 1400) & (df['nível'] == 'VIP')]
-BonusMp = df[(df['ponto gera'] > 1000) & (df['nível'] == 'MP')]
-print(f' Os executivos MP que bateram o bonus são: {BonusMp[['executivos', 'ponto gera']].to_string(header=False)}')
-print(f'VIPs que ganharam bonus: {BonusVip[['executivos', 'ponto gera']].to_string(header=False)}')
+
+BonusGeraMP = df[(df['ponto gera'] > 1000) & (df['nível'] == 'MP')]
+print('Bônus Meta GERA MP:')
+print(BonusGeraMP[['executivo','ponto gera']])
+print('-' * 25)
+BonusGeraVIP = df[(df['ponto gera'] > 1400) & (df['nível'] == 'VIP')]
+print('Bônus Meta GERA VIP:')
+print(BonusGeraVIP[['executivo','ponto gera']])
+print('-' * 25)
+BonusM1Vip = df[(df['m1'] >= df['m1 meta']) & (df['nível'] == 'VIP')]
+print('Bônus Meta M1 VIP:')
+print(BonusM1Vip[['executivo','m1', 'm1 meta']])
+print('-' * 25)
+BonusM1MP = df[(df['m1'] >= df['m1 meta']) & (df['nível'] == 'MP')]
+print('Bônus Meta M1 VIP:')
+print(BonusM1MP[['executivo','m1', 'm1 meta']])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
