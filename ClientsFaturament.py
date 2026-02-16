dc9b1d3d0e725dfc5e4cf3cf94907fb6bcb88009
Samuel Matias <samuelmatiasbrito@gmail.com>
2026-02-16 11:55:20 -0300

    faturamento geral removido

--- ClientsFaturament.py
+++ ClientsFaturament.py
@@ -3,14 +3,17 @@ PoloGeraJaneiro = pd.read_excel('polosaomiguel.xlsx')
 ClientesJaneiro = pd.read_excel('faturamentoclientes.xlsx')
 
 class Gera:
-    def obterfaturamento(self):
-        return ClientesJaneiro[['executivo', 'id cliente', 'faturamento total']]
-    def filtrarfaturamento(self, filtro):
+    def __init__(self, faturamento, gera):
+        self.faturamento = self.LerArquivo(faturamento)
+        self.gera = self.LerArquivo(gera)
+    def LerArquivo(self,arquivo):
+        return pd.read_excel(arquivo)
+    def FiltrarFaturamento(self, filtro):
         return ClientesJaneiro[ClientesJaneiro['faturamento total'] > filtro]
-    def obtergera(self):
+    def ObterGera(self):
         return PoloGeraJaneiro
-    def obterm1(self):
+    def ObterM1(self):
         return PoloGeraJaneiro[['executivo','nível', 'm1', 'm1 meta']]
-    def obterexecutivos(self):
+    def ObterExecutivos(self):
         return '\n'.join([f'({i}) {executivo}' for i, executivo in enumerate(PoloGeraJaneiro['executivo'], start=1)])
     
--- pandastest.py
+++ pandastest.py
@@ -6,31 +6,26 @@ while True:
                      'Qual consulta você quer realizar? \n' \
 '(1) Faturamento dos clientes | (2) Base Gera | (3) Metas M1 | (4) Sair: ')
     if Consulta == '1':
-        ConsultaFaturamento = input('(1) Base Faturamento Geral \n' \
-        '(2) Base Faturamento com valor filtrado: ')
-        if ConsultaFaturamento == '1':
-            print(ConsultaGera.obterfaturamento())
-            pass
-        elif ConsultaFaturamento == '2':
-            FiltroFaturamento = int(input('Mínimo de faturamento: R$'))
-            print(ConsultaGera.filtrarfaturamento(FiltroFaturamento))
-            Export = input('Exportar para uma planilha Excel externa? (1) Sim | (2) Não: ')
-            if Export == '1':
-                NomeArquivo = input('Nome arquivo: ')
-                NomePlanilha = input('Nome Planilha: ')
-                print('salvando...')
-                ConsultaGera.filtrarfaturamento().to_excel(f'{NomeArquivo}.xlsx', sheet_name=NomePlanilha, index=False)
-            pass
+        FiltroFaturamento = int(input('Mínimo de faturamento: R$'))
+        print(ConsultaGera.FiltrarFaturamento(FiltroFaturamento))
+        Export = input('Exportar para uma planilha Excel externa? (1) Sim | (2) Não: ')
+        if Export == '1':
+            NomeArquivo = input('Nome arquivo: ')
+            NomePlanilha = input('Nome Planilha: ')
+            print('salvando...')
+            ConsultaGera.FiltrarFaturamento().to_excel(f'{NomeArquivo}.xlsx', sheet_name=NomePlanilha, index=False)
+        pass
     elif Consulta == '2':
         FiltrarExecutivo = input('Deseja filtrar algum executivo? (1) Sim | (2) Não: ')
         if FiltrarExecutivo == '1':
-            print(ConsultaGera.obterexecutivos())
+            print(ConsultaGera.ObterExecutivos())
+            
 
         elif FiltrarExecutivo == '2':
-            print(ConsultaGera.obtergera())
+            print(ConsultaGera.ObterGera())
         pass
     elif Consulta == '3':
-        print(ConsultaGera.obterm1())
+        print(ConsultaGera.ObterM1())
         pass
     elif Consulta == '4':
         print('Saindo do Gera...')
